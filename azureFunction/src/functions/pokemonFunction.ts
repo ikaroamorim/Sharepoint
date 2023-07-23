702f3e8fb959a8b280379d6baf1f676768aea174
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2023-07-23 18:14:40 -0300

    pokemon Functions: Trabalhando com lista

--- azureFunction/src/functions/pokemonFunction.ts
+++ azureFunction/src/functions/pokemonFunction.ts
@@ -6,29 +6,25 @@ import {
     formatPokemonDetail
 } from "./apiFunctions";
 
-import { getSPAuth } from "./spFunctions";
-
-
-
-
+import { getSPAuth, getSPItems } from "./spFunctions";
 
 export async function pokemonFunction(myTimer: Timer, context: InvocationContext): Promise<void> {
     const authHeaders = await getSPAuth(`${process.env["SITEURL"]}`)
 
+    console.log(authHeaders)
+
+    const siteItems = await getSPItems(`${process.env["SITEURL"]}`,`${process.env["LISTGUID"]}`, authHeaders.headers  )
+
+
+    /*
     const pokemonList = await getPokemons()
 
     pokemonList.results.forEach(async pokemonItem => {
         const pkmnDetail = formatPokemonDetail(await getPokemonDetail(pokemonItem.url))
 
         console.log(pkmnDetail)
-
-        //console.log( await getPokemonImages(pkmnDetail))
     });
-
-
-
-
-
+    */
 }
 
 app.timer('pokemonFunction', {
